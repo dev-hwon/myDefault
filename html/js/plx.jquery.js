11c593a8e9e6eff91a66946a13e660fc6aa5969f
tester_hyun <thehyun1@nate.com>
2022-05-31 13:54:25 +0900

    패럴랙스커스텀js 추가수정

--- html/js/plx.jquery.js
+++ html/js/plx.jquery.js
@@ -1,39 +1,35 @@
-(function($) {
-    $(function() {
-        function customPlx(){
-            $('.plx').each(function(index, item){
-                var elemTop = $(item).offset().top;
-                var type = $(item).data('plxtype'); 
+function customPlx(){
+    $('.plx').each(function(index, item){
+        var elemTop = $(item).offset().top;
+        var type = $(item).data('plxtype'); 
 
-                //적용타입이 배경일경우에만 적용
-                if(type === 'bg') {
-                    $(this).css({
-                        'background-repeat':'no-repeat',
-                        'background-attachment': 'fixed',
-                        'background-position':' 0 0'
-                    });
-                }
-                
-                // var elemBottom = elemTop + $(item).height();
-                $(window).scroll(function(){
-                    var docViewTop = $(window).scrollTop();
-                    var docViewBottom = docViewTop + $(window).height();
-                    // 배경에 적용시
-                    if(type === 'bg') { 
-                        var unit = 0.5*-1; // 무조건 - 값되게
-                        var pos_bg = ((docViewTop - elemTop) * unit); 
-                        if(docViewBottom >= elemTop) {
-                            $(item).css('background-position-y', pos_bg);
-                        }
-                    } else if(type ==='item') {  // 돔객체에 적용시
-                        var unit = $(item).data('plxspeed')*-1;
-                        var pos_item = ((docViewTop * unit) + elemTop);
-                        $(item).css('top', pos_item);
-                    }
-                });
-
+        //적용타입이 배경일경우에만 적용
+        if(type === 'bg') {
+            $(this).css({
+                'background-repeat':'no-repeat',
+                'background-attachment': 'fixed',
+                'background-position':' 0 0'
             });
         }
-        customPlx();
+        
+        // var elemBottom = elemTop + $(item).height();
+        $(window).scroll(function(){
+            var docViewTop = $(window).scrollTop();
+            var docViewBottom = docViewTop + $(window).height();
+            // 배경에 적용시
+            if(type === 'bg') { 
+                var unit = 0.5*-1; // 무조건 - 값되게
+                var pos_bg = ((docViewTop - elemTop) * unit); 
+                if(docViewBottom >= elemTop) {
+                    $(item).css('background-position-y', pos_bg);
+                }
+            } else if(type ==='item') {  // 돔객체에 적용시
+                var unit = $(item).data('plxspeed')*-1;
+                var pos_item = ((docViewTop * unit) + elemTop);
+                $(item).css('top', pos_item);
+            }
+        });
+
     });
-})(jQuery);
+}
+customPlx();
