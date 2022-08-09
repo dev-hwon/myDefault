6f4eac2fc18f539e48848b2e48e1d7779fd4fd5a
tester_hyun <thehyun1@nate.com>
2022-08-09 14:25:33 +0900

    custom input=range 단방향,양방향 개선

--- html/a/js/customRange.js
+++ html/a/js/customRange.js
@@ -1,21 +1,26 @@
-
+// 단방향
 function oneWayRange(target) {
     const oneWayRange = document.querySelector(target);
     const input = oneWayRange.querySelector(".input_range");
     const thumb = oneWayRange.querySelector(".thumb");
     const range = oneWayRange.querySelector(".range");
-    // step은 한점에서 만나는경우가 없을경우 사용..
     const setValue = e => {
         const _this = e.target;
-        const {value, min, max } = _this;  
+        const { min, max } = _this;  
         const percent = ((+_this.value - +min) / (+max - +min)) * 100;
-        thumb.innerHTML = "<span>" + _this.value; + "</span>";
+        thumb.querySelector('span').innerHTML = addComma(_this.value);
         thumb.style.right = `${100 - percent}%`;
-        range.style.right = `${100 - percent}%`;
+        range.style.right = `${100 - percent}%`; 
+        if((100 - percent) < 5) {
+            thumb.querySelector('span').classList.add('edge');
+        } else {
+            thumb.querySelector('span').classList.remove('edge');
+        }
     };
     input.addEventListener("input", setValue);
 }
 
+// 양방향 레인지
 function rangeBothSide(target) {
     const rangeBothSide = document.querySelector(target);
     const inputLeft = rangeBothSide.querySelector(".input_left");
@@ -23,28 +28,38 @@ function rangeBothSide(target) {
     const thumbLeft = rangeBothSide.querySelector(".thumb.left");
     const thumbRight = rangeBothSide.querySelector(".thumb.right");
     const range = rangeBothSide.querySelector(".range");
-    // step은 한점에서 만나는경우가 없을경우 사용..
     const setLeftValue = e => {
         const _this = e.target;
-        const {value, min, max } = _this;  
-        if (+inputRight.value - +value <= 0) {
-            _this.value = +inputRight.value;
+        const {step, value, min, max } = _this;  
+        if (+inputRight.value - +value <= step) {
+            _this.value = +inputRight.value - step;
         }
         const percent = ((+_this.value - +min) / (+max - +min)) * 100;
-        thumbLeft.innerHTML = "<span>" + _this.value; + "</span>";
+		thumbLeft.querySelector('span').innerHTML = addComma(_this.value);
         thumbLeft.style.left = `${percent}%`;
         range.style.left = `${percent}%`;
+		if((percent) < 5) {
+			thumbLeft.querySelector('span').classList.add('edge');
+		} else {
+			thumbLeft.querySelector('span').classList.remove('edge');
+		}
     };
     const setRightValue = e => {
         const _this = e.target;
-        const { value, min, max } = _this;
-        if (+value - +inputLeft.value < 10) {
-            _this.value = +inputLeft.value + 10;
+        const {step, value, min, max } = _this;
+        if (+value - +inputLeft.value <= step) {
+            _this.value = +inputLeft.value + +step;
         }
         const percent = ((+_this.value - +min) / (+max - +min)) * 100;
-        thumbRight.innerHTML = "<span>" + _this.value; + "</span>";
+        thumbRight.querySelector('span').innerHTML = addComma(_this.value);
         thumbRight.style.right = `${100 - percent}%`;
         range.style.right = `${100 - percent}%`;
+		if((100 - percent) < 5) {
+			thumbRight.querySelector('span').classList.add('edge');
+			// thumbRight.querySelector('span').animate({transform: ['translateX(-100%)'], left : "100%"}, { duration: 500, fill: 'forwards'});
+		} else {
+			thumbRight.querySelector('span').classList.remove('edge');
+		}
     };
     if (inputLeft && inputRight) {
         inputLeft.addEventListener("input", setLeftValue);
